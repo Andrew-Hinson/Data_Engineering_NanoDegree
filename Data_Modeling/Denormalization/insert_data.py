df0ddc090e4822dabfca97ba319772b52b8e6710
andrew hinson <a.hinson@fetchrewards.com>
2022-06-01 22:23:40 -0500

    [denorm] insert data script created

---
+++ Data_Modeling/Denormalization/insert_data.py
@@ -0,0 +1,150 @@
+# # TO-DO: Insert data into the tables    
+import psycopg2
+import PIvariables
+# extablish connection to database and set variables connection and cursur
+
+p = PIvariables.password
+
+try: 
+    conn = psycopg2.connect("host=localhost dbname=music user=postgres password={}".format(p))
+    print('connected to new database')
+except psycopg2.Error as e: 
+    print("Error: Could not make connection to the Postgres database")
+    print(e)
+
+try: 
+    cur = conn.cursor()
+except psycopg2.Error as e: 
+    print("Error: Could not get curser to the Database")
+    print(e)
+
+conn.set_session(autocommit=True)  
+    
+
+
+
+# transactions data
+
+try: 
+    cur.execute("INSERT INTO transactions2 (transaction_id, customer_name, cashier_id, year) \
+                 VALUES (%s, %s, %s, %s)", \
+                 (1, "Amanda", 1, 2000))
+except psycopg2.Error as e: 
+    print("Error: Inserting Rows")
+    print (e)
+
+try: 
+    cur.execute("INSERT INTO transactions2 (transaction_id, customer_name, cashier_id, year) \
+                 VALUES (%s, %s, %s, %s)", \
+                 (2, "Toby", 1, 2000))
+except psycopg2.Error as e: 
+    print("Error: Inserting Rows")
+    print (e)
+    
+try: 
+    cur.execute("INSERT INTO transactions2 (transaction_id, customer_name, cashier_id, year) \
+                 VALUES (%s, %s, %s, %s)", \
+                 (3, "Max", 2, 2018))
+except psycopg2.Error as e: 
+    print("Error: Inserting Rows")
+    print (e)
+    
+
+# albums sold data
+try: 
+    cur.execute("INSERT INTO albums_sold (album_id, transaction_id, album_name) \
+                 VALUES (%s, %s, %s)", \
+                 (1, 1, "Rubber Soul"))
+except psycopg2.Error as e: 
+    print("Error: Inserting Rows")
+    print (e)
+
+try: 
+    cur.execute("INSERT INTO albums_sold (album_id, transaction_id, album_name) \
+                 VALUES (%s, %s, %s)", \
+                 (2, 1, "Let It Be"))
+except psycopg2.Error as e: 
+    print("Error: Inserting Rows")
+    print (e)
+    
+try: 
+    cur.execute("INSERT INTO albums_sold (album_id, transaction_id, album_name) \
+                 VALUES (%s, %s, %s)", \
+                 (3, 2, "My Generation"))
+except psycopg2.Error as e: 
+    print("Error: Inserting Rows")
+    print (e)
+    
+try: 
+    cur.execute("INSERT INTO albums_sold (album_id, transaction_id, album_name) \
+                 VALUES (%s, %s, %s)", \
+                 (4, 3, "Meet the Beatles"))
+except psycopg2.Error as e: 
+    print("Error: Inserting Rows")
+    print (e)
+
+try: 
+    cur.execute("INSERT INTO albums_sold (album_id, transaction_id, album_name) \
+                 VALUES (%s, %s, %s)", \
+                 (5, 3, "Help!"))
+except psycopg2.Error as e: 
+    print("Error: Inserting Rows")
+    print (e)
+
+# employee data
+try: 
+    cur.execute("INSERT INTO employees (employee_id, employee_name) \
+                 VALUES (%s, %s)", \
+                 (1, "Sam"))
+except psycopg2.Error as e: 
+    print("Error: Inserting Rows")
+    print (e)
+
+try: 
+    cur.execute("INSERT INTO employees (employee_id, employee_name) \
+                 VALUES (%s, %s)", \
+                 (2, "Bob"))
+except psycopg2.Error as e: 
+    print("Error: Inserting Rows")
+    print (e)    
+    
+# sales data
+try: 
+    cur.execute("INSERT INTO sales (transaction_id, amount_spent) \
+                 VALUES (%s, %s)", \
+                 (1, 40))
+except psycopg2.Error as e: 
+    print("Error: Inserting Rows")
+    print (e)    
+    
+try: 
+    cur.execute("INSERT INTO sales (transaction_id, amount_spent) \
+                 VALUES (%s, %s)", \
+                 (2, 19))
+except psycopg2.Error as e: 
+    print("Error: Inserting Rows")
+    print (e) 
+
+try: 
+    cur.execute("INSERT INTO sales (transaction_id, amount_spent) \
+                 VALUES (%s, %s)", \
+                 (3, 45))
+except psycopg2.Error as e: 
+    print("Error: Inserting Rows")
+    print (e) 
+
+# Close connection to database
+
+try:
+    cur.close()
+    print('closed curser')
+except psycopg2.Error as e:
+    print("Error: could not close curser")
+    print(e)
+
+try:
+    conn.close()
+    print('closed connection')
+except psycopg2.Error as e:
+    print('Error: could not close connection')
+    print(e)
